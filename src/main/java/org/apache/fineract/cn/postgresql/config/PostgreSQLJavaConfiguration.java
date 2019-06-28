96df860f172e4aafc0a804c87a3d3c4b0dad0ac3
Ebenezer Graham <egraham15@alustudent.com>
2019-06-28 06:15:17 +0400

    Setup Runtime Enhancement

--- src/main/java/org/apache/fineract/cn/postgresql/config/PostgreSQLJavaConfiguration.java
+++ src/main/java/org/apache/fineract/cn/postgresql/config/PostgreSQLJavaConfiguration.java
@@ -55,103 +55,95 @@ import java.util.Properties;
 @ConditionalOnProperty(prefix = "postgresql", name = "enabled", matchIfMissing = true)
 @EnableTransactionManagement
 @EnableApplicationName
-public class PostgreSQLJavaConfiguration  extends JpaBaseConfiguration {
-
-  private final Environment env;
-  
-  @Autowired
-  protected PostgreSQLJavaConfiguration(final Environment env,DataSource dataSource, OpenJpaProperties properties,
-                          ObjectProvider<JtaTransactionManager> jtaTransactionManager) {
-    super(dataSource, properties, jtaTransactionManager);
-    this.env = env;
-  
-  }
-
-  @Bean(name = PostgreSQLConstants.LOGGER_NAME)
-  public Logger logger() {
-    return LoggerFactory.getLogger(PostgreSQLConstants.LOGGER_NAME);
-  }
-
-  @Bean
-  public LocalContainerEntityManagerFactoryBean entityManagerFactory(@SuppressWarnings("SpringJavaAutowiringInspection") final DataSource dataSource) {
-    final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    em.setPersistenceUnitName("metaPU");
-    em.setJtaDataSource(dataSource);
-    em.setDataSource(dataSource);
-    em.setPackagesToScan("org.apache.fineract.cn.**.repository");
-    
-    final JpaVendorAdapter jpaVendorAdapter = new OpenJpaVendorAdapter();
-    em.setJpaVendorAdapter(jpaVendorAdapter);
-    em.setJpaProperties(additionalProperties());
-    return em;
-  }
-
-  @Bean
-  public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-    final JpaTransactionManager transactionManager = new JpaTransactionManager();
-    transactionManager.setEntityManagerFactory(emf);
-    return transactionManager;
-  }
-
-  @Bean
-  public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-    return new PersistenceExceptionTranslationPostProcessor();
-  }
-
-  @Bean
-  public FlywayFactoryBean flywayFactoryBean(final ApplicationName applicationName) {
-    return new FlywayFactoryBean(applicationName);
-  }
-
-  @Bean
-  public MetaDataSourceWrapper metaDataSourceWrapper() {
-
-    final BoneCPDataSource boneCPDataSource = new BoneCPDataSource();
-    boneCPDataSource.setDriverClass(
-            this.env.getProperty(PostgreSQLConstants.POSTGRESQL_DRIVER_CLASS_PROP, PostgreSQLConstants.POSTGRESQL_DRIVER_CLASS_DEFAULT));
-    boneCPDataSource.setJdbcUrl(JdbcUrlBuilder
-            .create(JdbcUrlBuilder.DatabaseType.POSTGRESQL)
-            .host(this.env.getProperty(PostgreSQLConstants.POSTGRESQL_HOST_PROP, PostgreSQLConstants.POSTGRESQL_HOST_DEFAULT))
-            .port(this.env.getProperty(PostgreSQLConstants.POSTGRESQL_PORT_PROP, PostgreSQLConstants.POSTGRESQL_PORT_DEFAULT))
-            .instanceName(this.env.getProperty(PostgreSQLConstants.POSTGRESQL_DATABASE_NAME_PROP, PostgreSQLConstants.POSTGRESQL_DATABASE_NAME_DEFAULT))
-            .build());
-    boneCPDataSource.setUsername(
-            this.env.getProperty(PostgreSQLConstants.POSTGRESQL_USER_PROP, PostgreSQLConstants.POSTGRESQL_USER_DEFAULT));
-    boneCPDataSource.setPassword(
-            this.env.getProperty(PostgreSQLConstants.POSTGRESQL_PASSWORD_PROP, PostgreSQLConstants.POSTGRESQL_PASSWORD_DEFAULT));
-    boneCPDataSource.setIdleConnectionTestPeriodInMinutes(
-            Long.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_IDLE_CONNECTION_TEST_PROP, PostgreSQLConstants.BONECP_IDLE_CONNECTION_TEST_DEFAULT)));
-    boneCPDataSource.setIdleMaxAgeInMinutes(
-            Long.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_IDLE_MAX_AGE_PROP, PostgreSQLConstants.BONECP_IDLE_MAX_AGE_DEFAULT)));
-    boneCPDataSource.setMaxConnectionsPerPartition(
-            Integer.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_MAX_CONNECTION_PARTITION_PROP, PostgreSQLConstants.BONECP_MAX_CONNECTION_PARTITION_DEFAULT)));
-    boneCPDataSource.setMinConnectionsPerPartition(
-            Integer.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_MIN_CONNECTION_PARTITION_PROP, PostgreSQLConstants.BONECP_MIN_CONNECTION_PARTITION_DEFAULT)));
-    boneCPDataSource.setPartitionCount(
-            Integer.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_PARTITION_COUNT_PROP, PostgreSQLConstants.BONECP_PARTITION_COUNT_DEFAULT)));
-    boneCPDataSource.setAcquireIncrement(
-            Integer.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_ACQUIRE_INCREMENT_PROP, PostgreSQLConstants.BONECP_ACQUIRE_INCREMENT_DEFAULT)));
-    boneCPDataSource.setStatementsCacheSize(
-            Integer.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_STATEMENT_CACHE_PROP, PostgreSQLConstants.BONECP_STATEMENT_CACHE_DEFAULT)));
-
-    final Properties driverProperties = new Properties();
-    driverProperties.setProperty("useServerPrepStmts", "false");
-    boneCPDataSource.setDriverProperties(driverProperties);
-    return new MetaDataSourceWrapper(boneCPDataSource);
-  }
-
-  private Properties additionalProperties() {
-    final Properties properties = new Properties();
-    return properties;
-  }
-  
-  @Override
-  protected AbstractJpaVendorAdapter createJpaVendorAdapter() {
-    return new OpenJpaVendorAdapter();
-  }
-  
-  @Override
-  protected Map<String, Object> getVendorProperties() {
-    return new HashMap<>(0);
-  }
+public class PostgreSQLJavaConfiguration {
+	
+	private final Environment env;
+	
+	@Autowired
+	protected PostgreSQLJavaConfiguration(final Environment env) {
+		super();
+		this.env = env;
+		
+	}
+	
+	@Bean(name = PostgreSQLConstants.LOGGER_NAME)
+	public Logger logger() {
+		return LoggerFactory.getLogger(PostgreSQLConstants.LOGGER_NAME);
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(@SuppressWarnings("SpringJavaAutowiringInspection") final DataSource dataSource) {
+		final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setPersistenceUnitName("metaPU");
+		em.setDataSource(dataSource);
+		em.setPackagesToScan("org.apache.fineract.cn.**.repository");
+		
+		final JpaVendorAdapter jpaVendorAdapter = new OpenJpaVendorAdapter();
+		em.setJpaVendorAdapter(jpaVendorAdapter);
+		em.setJpaProperties(additionalProperties());
+		return em;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		final JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
+	
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+	
+	@Bean
+	public FlywayFactoryBean flywayFactoryBean(final ApplicationName applicationName) {
+		return new FlywayFactoryBean(applicationName);
+	}
+	
+	@Bean
+	public MetaDataSourceWrapper metaDataSourceWrapper() {
+		
+		final BoneCPDataSource boneCPDataSource = new BoneCPDataSource();
+		boneCPDataSource.setDriverClass(
+				this.env.getProperty(PostgreSQLConstants.POSTGRESQL_DRIVER_CLASS_PROP, PostgreSQLConstants.POSTGRESQL_DRIVER_CLASS_DEFAULT));
+		boneCPDataSource.setJdbcUrl(JdbcUrlBuilder
+				.create(JdbcUrlBuilder.DatabaseType.POSTGRESQL)
+				.host(this.env.getProperty(PostgreSQLConstants.POSTGRESQL_HOST_PROP, PostgreSQLConstants.POSTGRESQL_HOST_DEFAULT))
+				.port(this.env.getProperty(PostgreSQLConstants.POSTGRESQL_PORT_PROP, PostgreSQLConstants.POSTGRESQL_PORT_DEFAULT))
+				.instanceName(this.env.getProperty(PostgreSQLConstants.POSTGRESQL_DATABASE_NAME_PROP, PostgreSQLConstants.POSTGRESQL_DATABASE_NAME_DEFAULT))
+				.build());
+		boneCPDataSource.setUsername(
+				this.env.getProperty(PostgreSQLConstants.POSTGRESQL_USER_PROP, PostgreSQLConstants.POSTGRESQL_USER_DEFAULT));
+		boneCPDataSource.setPassword(
+				this.env.getProperty(PostgreSQLConstants.POSTGRESQL_PASSWORD_PROP, PostgreSQLConstants.POSTGRESQL_PASSWORD_DEFAULT));
+		boneCPDataSource.setIdleConnectionTestPeriodInMinutes(
+				Long.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_IDLE_CONNECTION_TEST_PROP, PostgreSQLConstants.BONECP_IDLE_CONNECTION_TEST_DEFAULT)));
+		boneCPDataSource.setIdleMaxAgeInMinutes(
+				Long.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_IDLE_MAX_AGE_PROP, PostgreSQLConstants.BONECP_IDLE_MAX_AGE_DEFAULT)));
+		boneCPDataSource.setMaxConnectionsPerPartition(
+				Integer.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_MAX_CONNECTION_PARTITION_PROP, PostgreSQLConstants.BONECP_MAX_CONNECTION_PARTITION_DEFAULT)));
+		boneCPDataSource.setMinConnectionsPerPartition(
+				Integer.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_MIN_CONNECTION_PARTITION_PROP, PostgreSQLConstants.BONECP_MIN_CONNECTION_PARTITION_DEFAULT)));
+		boneCPDataSource.setPartitionCount(
+				Integer.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_PARTITION_COUNT_PROP, PostgreSQLConstants.BONECP_PARTITION_COUNT_DEFAULT)));
+		boneCPDataSource.setAcquireIncrement(
+				Integer.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_ACQUIRE_INCREMENT_PROP, PostgreSQLConstants.BONECP_ACQUIRE_INCREMENT_DEFAULT)));
+		boneCPDataSource.setStatementsCacheSize(
+				Integer.valueOf(this.env.getProperty(PostgreSQLConstants.BONECP_STATEMENT_CACHE_PROP, PostgreSQLConstants.BONECP_STATEMENT_CACHE_DEFAULT)));
+		
+		final Properties driverProperties = new Properties();
+		driverProperties.setProperty("useServerPrepStmts", "false");
+		boneCPDataSource.setDriverProperties(driverProperties);
+		return new MetaDataSourceWrapper(boneCPDataSource);
+	}
+	
+	private Properties additionalProperties() {
+		final Properties properties = new Properties();
+		properties.setProperty("openjpa.jdbc.DBDictionary", "org.apache.openjpa.jdbc.sql.PostgresDictionary");
+		properties.setProperty("openjpa.RuntimeUnenhancedClasses", "supported"); // set to unsupported for production
+		properties.setProperty("openjpa.DynamicEnhancementAgent", "true");
+		properties.setProperty("openjpa.Log", "DefaultLevel=TRACE, Tool=INFO, SQL=TRACE, Runtime=TRACE"); // set to unsupported for production
+		return properties;
+	}
 }
